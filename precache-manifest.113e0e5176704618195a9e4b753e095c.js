567efdc7c78404bfd50b37dd184d67b03e1c57eb
m.az94@hotmail.com <maria@marias-MacBook-Air.local>
2019-07-29 01:23:11 -0400

    Updates

---
+++ precache-manifest.113e0e5176704618195a9e4b753e095c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfb8c740cb57ad17e0fe0de1ea42e69",
+    "url": "/MariaAzam/index.html"
+  },
+  {
+    "revision": "4062ba8464cba4dc8cd0",
+    "url": "/MariaAzam/static/css/main.f818bf60.chunk.css"
+  },
+  {
+    "revision": "c0d3fe8276dd021ea14b",
+    "url": "/MariaAzam/static/js/2.6ea000ff.chunk.js"
+  },
+  {
+    "revision": "4062ba8464cba4dc8cd0",
+    "url": "/MariaAzam/static/js/main.3b8f19c9.chunk.js"
+  },
+  {
+    "revision": "ce156944fa54b35b5228",
+    "url": "/MariaAzam/static/js/runtime~main.cbf007bb.js"
+  },
+  {
+    "revision": "84c1eb74c76ff50ae4029cf96b22886d",
+    "url": "/MariaAzam/static/media/BG.84c1eb74.png"
+  },
+  {
+    "revision": "54ec5b69382ec9833818102733cbb464",
+    "url": "/MariaAzam/static/media/testimonial.54ec5b69.png"
+  }
+]);
